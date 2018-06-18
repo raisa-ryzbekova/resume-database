7ab7e17a9f832bcc435b6334e04da3b03084886b
Raisa Ryzbekova <raisa.ryzbekova@gmail.com>
2018-06-18 18:22:56 +0700

    add packages, getters, setters

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,14 +1,18 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 
 /**
- * Array based storage for Resumes
+ * Array based com.urise.webapp.model.storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = size();
+    private Resume[] storage = new Resume[10000];
+    private int size = size();
 
     // обнулить массив
-    void clear() {
+    public void clear() {
         for(int i = 0; i < size; i++) {
             storage[i] = null;
         }
@@ -16,24 +20,24 @@ public class ArrayStorage {
     }
 
     // сохранить резюме (вставка нового резюме за последним ненулевым объектом)
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
     // получить резюме по идентификатору (по значению поля)
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid)
+            if (storage[i].getUuid() == uuid)
                 return storage[i];
         }
         return null;
     }
 
     // удалить резюме, смещение оставшихся объектов по индексу на -1
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
+            if (storage[i].getUuid() == uuid) {
                 System.arraycopy(storage, i+1, storage, i, size-(i+1));
                 size--;
                 break;
@@ -42,15 +46,15 @@ public class ArrayStorage {
     }
 
     /**
-     * @return array, contains only Resumes in storage (without null)
+     * @return array, contains only Resumes in com.urise.webapp.model.storage (without null)
      */
     // получить копию массива сохраненных резюме (ненулевых объектов)
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
     // получить количество сохраненных резюме (ненулевых объектов)
-    int size() {
+    public int size() {
         return size;
     }
 }
