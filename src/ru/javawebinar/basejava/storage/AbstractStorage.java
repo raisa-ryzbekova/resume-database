33ac134eb8c42363b9d48e07717c6c61985f0256
Raisa Ryzbekova <raisa.ryzbekova@gmail.com>
2018-07-16 20:30:57 +0700

    add log to AbstractStorage

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,23 +6,29 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<K> implements Storage {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     @Override
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         K key = getNotExistedKey(resume.getUuid());
         toSave(key, resume);
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         K key = getExistedKey(uuid);
         return toGet(key);
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("Get all sorted");
         List<Resume> resumes = getAsList();
         Collections.sort(resumes);
         return resumes;
@@ -30,12 +36,14 @@ public abstract class AbstractStorage<K> implements Storage {
 
     @Override
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         K key = getExistedKey(resume.getUuid());
         toUpdate(key, resume);
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         K key = getExistedKey(uuid);
         toDelete(key);
     }
@@ -43,6 +51,7 @@ public abstract class AbstractStorage<K> implements Storage {
     private K getNotExistedKey(String uuid) {
         K key = getKey(uuid);
         if (isKeyExist(key)) {
+            LOG.warning("Resume with uuid = " + uuid + " already exists");
             throw new ExistStorageException(uuid);
         }
         return key;
@@ -51,6 +60,7 @@ public abstract class AbstractStorage<K> implements Storage {
     private K getExistedKey(String uuid) {
         K key = getKey(uuid);
         if (!isKeyExist(key)) {
+            LOG.warning("Resume with uuid = " + uuid + " doesn't exist");
             throw new NotExistStorageException(uuid);
         }
         return key;
