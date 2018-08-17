2327aad8586c6dd5896d55077cd33b23c5518ef3
Raisa Ryzbekova <raisa.ryzbekova@gmail.com>
2018-08-17 10:51:19 +0700

    refactor functional interfaces and lymbdas in DataStreamSerializer

--- src/ru/javawebinar/basejava/storage/serialization/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serialization/DataStreamSerializer.java
@@ -4,9 +4,7 @@ import ru.javawebinar.basejava.model.*;
 
 import java.io.*;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DataStreamSerializer implements StreamSerializer {
 
@@ -15,73 +13,55 @@ public class DataStreamSerializer implements StreamSerializer {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
-            writeCollection(dos, (dataOutputStream) -> {
-                Map<ContactType, String> contacts = resume.getContacts();
-                dos.writeInt(contacts.size());
-                for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                    dos.writeUTF(entry.getKey().name());
-                    dos.writeUTF(entry.getValue());
-                }
+            Map<ContactType, String> contacts = resume.getContacts();
+            writeCollection(dos, contacts.entrySet(), (elementCollection) -> {
+                dos.writeUTF(elementCollection.getKey().name());
+                dos.writeUTF(elementCollection.getValue());
             });
-            writeCollection(dos, (dataOutputStream) -> {
-                Map<SectionType, Section> sections = resume.getSections();
-                dos.writeInt(sections.size());
-                for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                    dos.writeUTF(entry.getKey().name());
-                    SectionType sectionType = entry.getKey();
-                    switch (sectionType) {
-                        case OBJECTIVE:
-                        case PERSONAL:
-                            dos.writeUTF(((TextSection) entry.getValue()).getSectionContent());
-                            break;
-                        case ACHIEVEMENT:
-                        case QUALIFICATIONS:
-                            writeCollection(dos, (dataOutputStream1) -> {
-                                List<String> listSectionContent = ((ListSection) entry.getValue()).getSectionContent();
-                                int sizeList = listSectionContent.size();
-                                dos.writeInt(sizeList);
-                                for (String aListSectionContent : listSectionContent) {
-                                    dos.writeUTF(aListSectionContent);
+            Map<SectionType, Section> sections = resume.getSections();
+            writeCollection(dos, sections.entrySet(), (elementCollection) -> {
+                dos.writeUTF(elementCollection.getKey().name());
+                SectionType sectionType = elementCollection.getKey();
+                switch (sectionType) {
+                    case OBJECTIVE:
+                    case PERSONAL:
+                        dos.writeUTF(((TextSection) elementCollection.getValue()).getSectionContent());
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        List<String> listSectionContent = ((ListSection) elementCollection.getValue()).getSectionContent();
+                        writeCollection(dos, listSectionContent, dos::writeUTF);
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        List<Company> companies = ((CompanySection) elementCollection.getValue()).getSectionContent();
+                        writeCollection(dos, companies, (elementCollection1) -> {
+                            dos.writeUTF(elementCollection1.getCompanyName());
+                            if (elementCollection1.getUrl() == null) {
+                                dos.writeUTF("");
+                            } else {
+                                dos.writeUTF(elementCollection1.getUrl());
+                            }
+                            List<Company.PositionInCompany> positionList = elementCollection1.getListOfPositions();
+                            writeCollection(dos, positionList, (elementInInScope) -> {
+                                dos.writeInt(elementInInScope.getStartDate().getYear());
+                                dos.writeInt(elementInInScope.getStartDate().getMonth().getValue());
+                                if (elementInInScope.getEndDate() == null) {
+                                    dos.writeInt(3000);
+                                    dos.writeInt(1);
+                                } else {
+                                    dos.writeInt(elementInInScope.getEndDate().getYear());
+                                    dos.writeInt(elementInInScope.getEndDate().getMonth().getValue());
+                                }
+                                dos.writeUTF(elementInInScope.getPosition());
+                                if (elementInInScope.getFunctions() == null) {
+                                    dos.writeUTF("");
+                                } else {
+                                    dos.writeUTF(elementInInScope.getFunctions());
                                 }
                             });
-                            break;
-                        case EXPERIENCE:
-                        case EDUCATION:
-                            writeCollection(dos, (dataOutputStream1Scope) -> {
-                                List<Company> companies = ((CompanySection) entry.getValue()).getSectionContent();
-                                dos.writeInt(companies.size());
-                                for (Company company : companies) {
-                                    dos.writeUTF(company.getCompanyName());
-                                    if (company.getUrl() == null) {
-                                        dos.writeUTF("");
-                                    } else {
-                                        dos.writeUTF(company.getUrl());
-                                    }
-                                    writeCollection(dos, (dataOutputStream2Scope) -> {
-                                        List<Company.PositionInCompany> positionList = company.getListOfPositions();
-                                        dos.writeInt(positionList.size());
-                                        for (Company.PositionInCompany position : positionList) {
-                                            dos.writeInt(position.getStartDate().getYear());
-                                            dos.writeInt(position.getStartDate().getMonth().getValue());
-                                            if (position.getEndDate() == null) {
-                                                dos.writeInt(3000);
-                                                dos.writeInt(1);
-                                            } else {
-                                                dos.writeInt(position.getEndDate().getYear());
-                                                dos.writeInt(position.getEndDate().getMonth().getValue());
-                                            }
-                                            dos.writeUTF(position.getPosition());
-                                            if (position.getFunctions() == null) {
-                                                dos.writeUTF("");
-                                            } else {
-                                                dos.writeUTF(position.getFunctions());
-                                            }
-                                        }
-                                    });
-                                }
-                            });
-                            break;
-                    }
+                        });
+                        break;
                 }
             });
         }
@@ -93,79 +73,65 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            readCollection(dis, (dataInputStream) -> {
-                int sizeContacts = dis.readInt();
-                for (int i = 0; i < sizeContacts; i++) {
-                    resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-                }
-            });
-            readCollection(dis, (dataInputStream) -> {
-                int sizeSections = dis.readInt();
-                for (int i = 0; i < sizeSections; i++) {
-                    String sectionType = dis.readUTF();
-                    switch (sectionType) {
-                        case "OBJECTIVE":
-                        case "PERSONAL":
-                            resume.setSection(SectionType.valueOf(sectionType), new TextSection(dis.readUTF()));
-                            break;
-                        case "ACHIEVEMENT":
-                        case "QUALIFICATIONS":
-                            readCollection(dis, (dataInputStream1Scope) -> {
-                                List<String> listSectionContent = new ArrayList<>();
-                                int sizeList = dis.readInt();
-                                for (int point = 0; point < sizeList; point++) {
-                                    listSectionContent.add(dis.readUTF());
-                                    resume.setSection(SectionType.valueOf(sectionType), new ListSection(listSectionContent));
-                                }
-                            });
-                            break;
-                        case "EXPERIENCE":
-                        case "EDUCATION":
-                            List<Link> linkList = new ArrayList<>();
-                            List<Company.PositionInCompany[]> positionArrayList = new ArrayList<>();
-                            readCollection(dis, (dataInputStream1Scope) -> {
-                                int sizeCompanyList = dis.readInt();
-                                for (int j = 0; j < sizeCompanyList; j++) {
-                                    linkList.add(new Link(dis.readUTF(), dis.readUTF()));
-                                    readCollection(dis, (dataInputStream2Scope) -> {
-                                        int sizePositions = dis.readInt();
-                                        Company.PositionInCompany[] positionArray = new Company.PositionInCompany[sizePositions];
-                                        for (int k = 0; k < sizePositions; k++) {
-                                            positionArray[k] = new Company.PositionInCompany(dis.readInt(), Month.of(dis.readInt()),
-                                                    dis.readInt(), Month.of(dis.readInt()), dis.readUTF(), dis.readUTF());
-                                        }
-                                        positionArrayList.add(positionArray);
-                                    });
-                                }
-                                readCollection(dis, (dataInputStream2Scope) -> {
-                                    List<Company> companyList = new ArrayList<>();
-                                    for (int m = 0; m < sizeCompanyList; m++) {
-                                        companyList.add(new Company(linkList.get(m).getName(), linkList.get(m).getUrl(), positionArrayList.get(m)));
-                                    }
-                                    resume.setSection(SectionType.valueOf(sectionType), new CompanySection(companyList));
-                                });
-                            });
-                            break;
-                    }
+            readData(dis, () -> resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readData(dis, () -> {
+                String sectionType = dis.readUTF();
+                switch (sectionType) {
+                    case "OBJECTIVE":
+                    case "PERSONAL":
+                        resume.setSection(SectionType.valueOf(sectionType), new TextSection(dis.readUTF()));
+                        break;
+                    case "ACHIEVEMENT":
+                    case "QUALIFICATIONS":
+                        resume.setSection(SectionType.valueOf(sectionType), new ListSection(readCollection(dis, dis::readUTF)));
+                        break;
+                    case "EXPERIENCE":
+                    case "EDUCATION":
+                        resume.setSection(SectionType.valueOf(sectionType), new CompanySection(
+                                readCollection(dis, () -> new Company(
+                                        new Link(dis.readUTF(), dis.readUTF()),
+                                        readCollection(dis, () -> new Company.PositionInCompany(dis.readInt(), Month.of(dis.readInt()),
+                                                dis.readInt(), Month.of(dis.readInt()), dis.readUTF(), dis.readUTF()))))));
+                        break;
                 }
             });
             return resume;
         }
     }
 
-    interface WriteCollection {
-        void write(DataOutputStream dos) throws IOException;
+
+    private interface WriteCollection<T> {
+        void write(T t) throws IOException;
     }
 
-    void writeCollection(DataOutputStream dos, WriteCollection writeFunctionalInterface) throws IOException {
-        writeFunctionalInterface.write(dos);
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, WriteCollection<T> writeFunctionalInterface) throws IOException {
+        dos.writeInt(collection.size());
+        for (T collectionElement : collection) {
+            writeFunctionalInterface.write(collectionElement);
+        }
     }
 
-    interface ReadCollection {
-        void read(DataInputStream dis) throws IOException;
+    private interface ReadData {
+        void read() throws IOException;
     }
 
-    void readCollection(DataInputStream dis, ReadCollection readFunctionalInterface) throws IOException {
-        readFunctionalInterface.read(dis);
+    private void readData(DataInputStream dis, ReadData readFunctionalInterface) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            readFunctionalInterface.read();
+        }
+    }
+
+    private interface ReadCollection<T> {
+        T read() throws IOException;
+    }
+
+    private <T> List<T> readCollection(DataInputStream dis, ReadCollection<T> readFunctionalInterface) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>(size);
+        for (int point = 0; point < size; point++) {
+            list.add(readFunctionalInterface.read());
+        }
+        return list;
     }
 }
