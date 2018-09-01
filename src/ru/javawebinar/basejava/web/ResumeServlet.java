ed0c259e60aa2137939fe4bb82e9db6c05edff79
Raisa Ryzbekova <raisa.ryzbekova@gmail.com>
2018-09-02 00:36:25 +0700

    tune servlets

---
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes" : "Hello " + name + "!");
+    }
+}
