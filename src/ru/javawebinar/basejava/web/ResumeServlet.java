2ddbba0ae8023f9d4f5e71022e124e54eeb24036
Raisa Ryzbekova <raisa.ryzbekova@gmail.com>
2018-09-04 11:50:42 +0700

    realize ResumeServlet: add output of table with uuid, full name (not finished)

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,12 +1,19 @@
 package ru.javawebinar.basejava.web;
 
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.Storage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResumeServlet extends HttpServlet {
+
+    private Storage sqlStorage = Config.get().getStorage();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
@@ -15,7 +22,22 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes" : "Hello " + name + "!");
+        String uuid = request.getParameter("uuid");
+        if (uuid != null) {
+            response.getWriter().write(uuid + " " + sqlStorage.get(uuid).getFullName());
+        } else {
+            try (PrintWriter out = response.getWriter()) {
+                out.println("<html>");
+                out.println("<table border=\"1\">");
+                for (Resume resume : sqlStorage.getAllSorted()) {
+                    out.println("<tr>");
+                    out.println("<td>" + resume.getUuid() + "</td>");
+                    out.println("<td>" + resume.getFullName() + "</td>");
+                    out.println("</tr>");
+                }
+                out.println("/table");
+                out.println("</html>");
+            }
+        }
     }
 }
